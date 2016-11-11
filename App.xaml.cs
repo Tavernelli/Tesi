bc365fba06eff556dbb6721c63f41fbc85ed4637
andrea tavernelli <tave.a@hotmail.it>
2016-11-11 18:14:38 +0100

    tesi

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Microsoft.Samples.Kinect.BodyBasics
+{
+    using System;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ source/ClassifiedObject.cs
@@ -0,0 +1,86 @@
+﻿using System.Drawing;
+
+namespace Microsoft.Samples.Kinect.BodyBasics
+{
+    class ClassifiedObject
+    {
+
+        private Rectangle mRect;
+        private double mZ = 0;
+        private double mRot = 0;
+        private string mFeature = "";
+
+        public ClassifiedObject(){ }
+
+        //Object Rectangle
+        public Rectangle rectangle
+        {
+            set
+            {
+                mRect = value;
+            }
+            get
+            {
+                return mRect;
+            }
+        }
+
+        //Rescale rectangle
+        public void ScaleRectangle(int scale)
+        {
+            rectangle = new Rectangle(
+                rectangle.X * scale,
+                rectangle.Y * scale,
+                rectangle.Width * scale,
+                rectangle.Height * scale
+            );
+        }
+
+        //Object Z axis (depth)
+        public double z
+        {
+            set
+            {
+                mZ = value;
+            }
+            get
+            {
+                return mZ;
+            }
+        }
+
+        //Object rotation (on screen)
+        public double rotation
+        {
+            set
+            {
+                mRot = value;
+            }
+            get
+            {
+                return mRot;
+            }
+        }
+
+        //Info about object
+        public string feature
+        {
+            set
+            {
+                mFeature = value;
+            }
+            get
+            {
+                return mFeature;
+            }
+        }
+
+        // rectangle,z and rotation to 0
+        public void SetAllToZero()
+        {
+            this.mRect = new Rectangle();
+            this.mZ = 0;
+            this.mRot = 0;
+        }
+    }
+}
